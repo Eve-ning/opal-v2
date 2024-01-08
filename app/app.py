8c7401a6632db004e5d914e5b8430426b1dbffe1
Evening <johnchangqi@hotmail.com>
2024-01-08 08:26:32 +0800

    Update varnames

--- app/app.py
+++ app/app.py
@@ -31,10 +31,10 @@ def get_model_embeddings(
     _m: DeltaModel,
     path: str,
 ):
-    w_uid_rc = _m.uid_emb_rc.weight.detach().numpy().squeeze()
-    w_uid_ln = _m.uid_emb_ln.weight.detach().numpy().squeeze()
-    w_mid_rc = _m.mid_emb_rc.weight.detach().numpy().squeeze()
-    w_mid_ln = _m.mid_emb_ln.weight.detach().numpy().squeeze()
+    w_uid_rc = _m.uid_rc_emb.weight.detach().numpy().squeeze()
+    w_uid_ln = _m.uid_ln_emb.weight.detach().numpy().squeeze()
+    w_mid_rc = _m.mid_rc_emb.weight.detach().numpy().squeeze()
+    w_mid_ln = _m.mid_ln_emb.weight.detach().numpy().squeeze()
     ln_ratio = _m.ln_ratio_weights.detach().numpy().squeeze()
     df_mid = pd.DataFrame(
         [_m.mid_le.classes_, w_mid_rc, w_mid_ln, ln_ratio]
@@ -54,19 +54,44 @@ model_path = st.selectbox(
 )
 
 m = load_model(model_path)
+#
+# df_uid = (
+#     pd.Series(m.uid_le.classes_)
+#     .str.split("/", expand=True)
+#     .rename(columns={0: "name", 1: "year"})
+# )
+# st.dataframe(df_uid)
+# left, right = st.columns(2)
+# uid_name = left.selectbox("User", df_uid["name"].unique())
+# uid_year = right.selectbox(
+#     "Year", df_uid[df_uid["name"] == uid_name]["year"].unique()
+# )
+#
+# w_uid_rc, w_uid_ln, w_mid_rc, w_mid_ln, ln_ratio = get_model_embeddings(
+#     m, model_path
+# )
+#
+# w_uid_rc.shape
+# w_uid_ln.shape
+# w_mid_rc.shape
+# w_mid_ln.shape
+# ln_ratio.shape
 
-df_mid, df_uid = get_model_embeddings(m, model_path)
+# %%
 
-st.header("Map Embeddings")
-is_dans_only = st.checkbox("Dans Only", value=True)
-map_embeddings(df_mid, dans_only=is_dans_only)
+if True:
+    df_mid, df_uid = get_model_embeddings(m, model_path)
 
-st.header("Player Embeddings")
-player_embeddings(df_uid)
+    st.header("Map Embeddings")
+    is_dans_only = st.checkbox("Dans Only", value=True)
+    map_embeddings(df_mid, dans_only=is_dans_only)
 
-st.header("Delta to Accuracy Mapping")
-st.write(
-    "This function maps the difference between player and map "
-    "embeddings to accuracy."
-)
-delta_to_acc(m)
+    st.header("Player Embeddings")
+    player_embeddings(df_uid)  # [df_uid["uid"].str.startswith(uid)])
+
+    st.header("Delta to Accuracy Mapping")
+    st.write(
+        "This function maps the difference between player and map "
+        "embeddings to accuracy."
+    )
+    delta_to_acc(m)
