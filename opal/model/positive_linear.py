98c43bb0f5b38a6b34450832cda759aa79c1e739
Evening <dev_evening@hotmail.com>
2024-01-17 16:29:33 +0800

    Use softplus for constraint

--- opal/model/positive_linear.py
+++ opal/model/positive_linear.py
@@ -3,11 +3,10 @@ import torch
 from torch import nn
 
 
-class ExpLinear(nn.Linear):
+class PositiveLinear(nn.Linear):
     def __init__(self, in_features, out_features, bias=True):
         super().__init__(in_features, out_features, bias)
+        self.fn = torch.nn.Softplus()
 
     def forward(self, x):
-        return F.linear(
-            x, torch.distributions.Normal(0, 1).cdf(self.weight), self.bias
-        )
+        return F.linear(x, self.fn(self.weight), self.bias)
