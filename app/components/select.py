e8e29db653f461748c6c7aef0665c26702bf387a
Evening <dev_evening@hotmail.com>
2024-01-17 20:45:11 +0800

    Split select into user and map

--- app/components/select.py
+++ app/components/select.py
@@ -2,20 +2,28 @@ import streamlit as st
 import pandas as pd
 
 
-def select(
+def select_user(
     df_uid_name: pd.DataFrame,
     df_uid_year: pd.DataFrame,
-    df_mid_name: pd.DataFrame,
-    df_mid_speed: pd.DataFrame,
 ):
     left, right = st.columns(2)
     username = left.selectbox("User", df_uid_name.unique())
     useryear = right.select_slider(
         "Year", df_uid_year[df_uid_name == username].unique()
     )
+    return username, useryear
 
-    left, right = st.columns(2)
-    mapname = left.selectbox("Map", df_mid_name.unique())
+
+def select_map(
+    df_mid_name: pd.DataFrame,
+    df_mid_speed: pd.DataFrame,
+):
+    mapname = st.selectbox("Map", df_mid_name.unique())
     speed_options = df_mid_speed[df_mid_name == mapname].unique()
-    mapspeed = right.radio("Speed", speed_options, horizontal=True)
-    return username, useryear, mapname, mapspeed
+    mapspeed = st.radio(
+        "Speed",
+        speed_options,
+        format_func={"-1": "HT", "0": "NM", "1": "DT"}.get,
+        horizontal=True,
+    )
+    return mapname, mapspeed
