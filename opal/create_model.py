d20d602569e4ff4769d2e9bb3e3795977d6a4c7c
Evening <dev_evening@hotmail.com>
2024-01-17 16:33:34 +0800

    Update model architecture

--- opal/create_model.py
+++ opal/create_model.py
@@ -1,5 +1,9 @@
 import pytorch_lightning as pl
-from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks import (
+    ModelCheckpoint,
+    StochasticWeightAveraging,
+    LearningRateMonitor,
+)
 from pytorch_lightning.loggers import WandbLogger
 from prefect import flow, task
 
@@ -15,25 +19,29 @@ def train(trainer: pl.Trainer, dm: OsuDataModule, m: DeltaModel):
 
 @flow(name="Create Opal Model")
 def create_model():
-    dm = OsuDataModule(df=df_k(7), min_map_plays=50, min_user_plays=50)
+    dm = OsuDataModule(
+        df=df_k(7),
+        min_map_plays=50,
+        min_user_plays=50,
+        n_acc_quantiles=10000,
+    )
 
     m = DeltaModel(
         uid_le=dm.uid_le,
         mid_le=dm.mid_le,
         acc_qt=dm.acc_qt,
         ln_ratio_weights=dm.ln_ratio_weights,
-        rc_emb=1,
-        ln_emb=1,
-        rc_delta_emb=10,
-        ln_delta_emb=10,
+        rc_emb=2,
+        ln_emb=2,
     )
 
     trainer = pl.Trainer(
-        max_epochs=40,
+        max_epochs=50,
         accelerator="cpu",
         default_root_dir="checkpoints",
         callbacks=[
             ModelCheckpoint(monitor="val_loss", save_top_k=1, mode="min"),
+            LearningRateMonitor(),
         ],
         logger=WandbLogger(project="opal"),
     )
