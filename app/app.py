1d591939706d97b88c7d75aa5894c196f357e23b
Evening <dev_evening@hotmail.com>
2024-01-18 18:41:28 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import numpy as np
 import plotly.express as px
 
+from components.delta_to_acc import delta_to_acc
 from components.select import select_user, select_map
 
 PROJECT_DIR = Path(__file__).parents[1]
@@ -11,6 +12,7 @@ sys.path.append(PROJECT_DIR.as_posix())
 import pandas as pd
 import streamlit as st
 
+from opal.data import df_k
 from opal.model.delta_model import DeltaModel
 
 THIS_DIR = Path(__file__).parent
@@ -97,6 +99,9 @@ ar_uid_rc = np.array(df_uid_all["RC"].tolist())
 ar_uid_ln = np.array(df_uid_all["LN"].tolist())
 # %%
 
+with st.expander("Model Analysis"):
+    st.markdown("# Delta to Accuracy Mapping")
+    delta_to_acc(m)
 
 if False:
     with st.expander("Global Analysis"):
@@ -117,13 +122,10 @@ if False:
             "embeddings to accuracy."
         )
         delta_to_acc(m)
+
+
 # %%
-username, useryear = select_user(
-    df_uid_name=df_uid_all.index.get_level_values("name"),
-    df_uid_year=df_uid_all.index.get_level_values("year"),
-)
-
-
+@st.cache_data()
 def player_rank(df: pd.DataFrame, n_uid: int):
     left, right = st.columns(2)
     left.metric(
@@ -136,6 +138,7 @@ def player_rank(df: pd.DataFrame, n_uid: int):
     )
 
 
+@st.cache_data()
 def player_rank_hist(df: pd.DataFrame, username: str, useryear: str):
     st.plotly_chart(
         px.histogram(x=df).update_layout(
@@ -146,6 +149,7 @@ def player_rank_hist(df: pd.DataFrame, username: str, useryear: str):
     )
 
 
+@st.cache_data()
 def map_ranks(df: pd.DataFrame):
     st.markdown("Players with at least ...")
     r_b, r_a, r_s = st.columns(3)
@@ -155,6 +159,7 @@ def map_ranks(df: pd.DataFrame):
     r_s.metric("S Rank", f"{(df >= 0.95).mean():.2%}")
 
 
+@st.cache_data()
 def map_rank_hist(df: pd.DataFrame, mapname: str, mapspeed_str: str):
     st.plotly_chart(
         px.histogram(x=df)
@@ -170,30 +175,141 @@ def map_rank_hist(df: pd.DataFrame, mapname: str, mapspeed_str: str):
     )
 
 
-df_rank_uid = df_accs.rank(axis=0, ascending=False)
-df_rank_uid_i = df_rank_uid.loc[f"{username}/{useryear}"]
+with st.expander("User Analysis"):
+    username, useryear = select_user(
+        df_uid_name=df_uid_all.index.get_level_values("name"),
+        df_uid_year=df_uid_all.index.get_level_values("year"),
+    )
 
-player_rank(df_rank_uid_i, n_uid)
-player_rank_hist(df_rank_uid_i, username, useryear)
+    df_rank_uid = df_accs.rank(axis=0, ascending=False)
+    df_rank_uid_i = df_rank_uid.loc[f"{username}/{useryear}"]
 
-mapname, mapspeed = select_map(
-    df_mid_name=df_mid_all.index.get_level_values("name"),
-    df_mid_speed=df_mid_all.index.get_level_values("speed"),
+    player_rank(df_rank_uid_i, n_uid)
+    player_rank_hist(df_rank_uid_i, username, useryear)
+
+with st.expander("Map Analysis"):
+    mapname, mapspeed = select_map(
+        df_mid_name=df_mid_all.index.get_level_values("name"),
+        df_mid_speed=df_mid_all.index.get_level_values("speed"),
+    )
+    mapspeed_str = {"-1": "HT", "0": "NM", "1": "DT"}.get(mapspeed)
+
+    df_accs_mid = df_accs.loc[:, f"{mapname}/{mapspeed}"]
+
+    map_ranks(df_accs_mid)
+    map_rank_hist(df_accs_mid, mapname, mapspeed_str)
+
+# %%
+with st.expander("Star Rating Analysis"):
+    threshold = 0.9
+    df_accs_thres: pd.DataFrame = (
+        (df_accs >= threshold)
+        .mean(axis=0)
+        .rename("thres")
+        .rename_axis("mapname")
+        .to_frame()
+    )
+    st.plotly_chart(
+        px.histogram(df_accs_thres, nbins=100)
+        .update_layout(
+            title=f"{threshold:.2%} Threshold Distribution",
+            xaxis_title=f"% of Population above {threshold:.2%}",
+            yaxis_title="Count of Maps",
+            showlegend=False,
+        )
+        .update_xaxes(autorange="reversed")
+    )
+    st.markdown(
+        f"""
+        The distribution of how many players can achieve {threshold:.2%}. 
+        - The further to the right, the rarer the {threshold:.2%} of a map, 
+        which corresponds higher difficulty. 
+        - The higher the peak, the more maps are in that difficulty range.
+        """
+    )
+    st.info(
+        """
+        Setting a low threshold has a unique property of reducing the effects
+        of gimmicked maps. Most of them are designed to be **extremely**
+        difficult to achieve a high accuracy, however, are easy to achieve
+        a decent one.        
+        """
+    )
+# %%
+min_sr, max_sr = 2.5, 10
+
+df_accs_thres["new_sr"] = (max_sr - min_sr) * (
+    np.sin((1 - df_accs_thres["thres"]) * np.pi / 2)
+) + min_sr
+
+#
+# @st.cache_data()
+# def get_df(k=7):
+#     return df_k.fn(k)
+#
+#
+# # %%
+# df_sr = (
+#     get_df()
+#     .groupby("mapname")
+#     .agg({"sr": "first"})
+#     .rename(columns={"sr": "old_sr"})
+# )
+# df_sr.index += "/0"
+# df_sr = df_accs_thres.merge(df_sr, right_index=True, left_index=True)
+# df_sr["delta_sr"] = df_sr["new_sr"] - df_sr["old_sr"]
+# # %%
+# st.plotly_chart(px.histogram(df_sr, x="new_sr", nbins=100))
+# st.plotly_chart(px.histogram(df_sr, x="old_sr", nbins=100))
+# st.dataframe(df_sr.sort_values("delta_sr", ascending=False))
+#
+# # %%
+# df: pd.DataFrame = get_df()
+# # # %%
+# #
+# # a = (
+#     (
+#         df.assign(
+#             mid=lambda x: x["mapname"] + "/" + x["speed"].astype(str),
+#             uid=lambda x: x["username"] + "/" + x["year"].astype(str),
+#         )
+#         .groupby(["mid", "uid"])
+#         .agg({"accuracy": "mean"})
+#         .reset_index()
+#         .pivot(index="mid", columns="uid", values="accuracy")
+#     )
+#     .dropna(how="all", axis=0)
+#     .dropna(how="all", axis=1)
+# )
+
+# %%
+from opal.data import OsuDataModule, df_k
+
+dm = OsuDataModule(df_k(7))
+df = dm.df.groupby(["mid", "uid"]).agg({"accuracy": "mean"}).reset_index()
+# %%
+df_p = df.pivot(index="uid", columns="mid", values="accuracy")
+# %%
+df_err = accs - df_p
+df_m_err = (df_err.abs()).quantile(0.85, axis=0).rename("uncertainty")
+
+# We do this as we only have srs of NT
+df_m_err = df_m_err[df_m_err.index.str.endswith("/0")].to_frame()
+df_m_err["uncertainty_pct"] = df_m_err["uncertainty"].rank(pct=True)
+
+# %%
+df_sr = (
+    df[df["mid"].str.endswith("/0")]
+    .groupby("mid")
+    .agg({"sr": "first"})
+    .rename(columns={"sr": "old_sr"})
 )
-mapspeed_str = {"-1": "HT", "0": "NM", "1": "DT"}.get(mapspeed)
-
-df_accs_mid = df_accs.loc[:, f"{mapname}/{mapspeed}"]
-
-
-map_ranks(df_accs_mid)
-map_rank_hist(df_accs_mid, mapname, mapspeed_str)
-
+df_sr = df_accs_thres.merge(df_sr, right_index=True, left_index=True).merge(
+    df_m_err, right_index=True, left_index=True
+)
 # %%
-df_accs_s = (df_accs < 0.925).mean(axis=0)
+df_sr["delta_sr"] = df_sr["new_sr"] - df_sr["old_sr"]
 # %%
-MAX_SR = 10
-df_t = 1 / (df_accs_s + (1 / MAX_SR))
-# %%
-
-st.plotly_chart(px.histogram(df_accs_s, nbins=100))
-st.dataframe(df_t)
+st.plotly_chart(px.histogram(df_sr, x="new_sr", nbins=100))
+st.plotly_chart(px.histogram(df_sr, x="old_sr", nbins=100))
+st.dataframe(df_sr.sort_values("delta_sr", ascending=False))
