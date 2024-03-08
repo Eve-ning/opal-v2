875f1d502a8d37478b444b38396c69079cd0089a
Evening <dev_evening@hotmail.com>
2024-03-08 16:31:56 +0800

    Improve documentation of model

--- opal/model/delta_model.py
+++ opal/model/delta_model.py
@@ -4,7 +4,6 @@ import numpy as np
 import pandas as pd
 import pytorch_lightning as pl
 import torch
-from pytorch_lightning.utilities.types import STEP_OUTPUT
 from sklearn.preprocessing import LabelEncoder, QuantileTransformer
 from torch import nn
 from torch.nn.functional import softplus, hardsigmoid
@@ -15,101 +14,181 @@ from opal.model.positive_linear import PositiveLinear
 class DeltaModel(pl.LightningModule):
     def __init__(
         self,
-        uid_le: LabelEncoder,
-        mid_le: LabelEncoder,
-        acc_qt: QuantileTransformer,
-        ln_ratio_weights: list,
-        rc_emb: int = 1,
-        ln_emb: int = 1,
+        le_uid: LabelEncoder,
+        le_mid: LabelEncoder,
+        qt_acc: QuantileTransformer,
+        w_ln_ratio: list,
+        n_rc_emb: int = 1,
+        n_ln_emb: int = 1,
+        n_delta_neurons: int = 4,
+        lr: float = 0.003,
     ):
+        """Delta Model for Osu!Mania Score Prediction
+
+        Args:
+            le_uid: Label Encoder for User IDs
+            le_mid: Label Encoder for Beatmap IDs
+            qt_acc: Quantile Transformer for Accuracy
+            w_ln_ratio: Weights for LN Ratio
+            n_rc_emb: Number of Embeddings for Relative Coordinates
+            n_ln_emb: Number of Embeddings for Local Normals
+        """
         super().__init__()
-        self.uid_le = uid_le
-        self.mid_le = mid_le
-        self.acc_qt = acc_qt
-        self.ln_ratio_weights = nn.Parameter(
-            torch.tensor(ln_ratio_weights, dtype=torch.float),
+        self.le_uid = le_uid
+        self.le_mid = le_mid
+        self.qt_acc = qt_acc
+        self.w_ln_ratio = nn.Parameter(
+            torch.tensor(w_ln_ratio, dtype=torch.float),
             requires_grad=False,
         )
-        n_uid = len(uid_le.classes_)
-        n_mid = len(mid_le.classes_)
-        self.uid_rc_emb = nn.Embedding(n_uid, rc_emb)
-        self.mid_rc_emb = nn.Embedding(n_mid, rc_emb)
-        self.uid_ln_emb = nn.Embedding(n_uid, ln_emb)
-        self.mid_ln_emb = nn.Embedding(n_mid, ln_emb)
-        self.rc_emb_bn = nn.BatchNorm1d(rc_emb, affine=False)
-        self.ln_emb_bn = nn.BatchNorm1d(ln_emb, affine=False)
+        self.lr = lr
 
+        n_uid = len(le_uid.classes_)
+        n_mid = len(le_mid.classes_)
+
+        # Embeddings for User and Beatmap IDs
+        self.emb_uid_rc = nn.Embedding(n_uid, n_rc_emb)
+        self.emb_mid_rc = nn.Embedding(n_mid, n_rc_emb)
+        self.emb_uid_ln = nn.Embedding(n_uid, n_ln_emb)
+        self.emb_mid_ln = nn.Embedding(n_mid, n_ln_emb)
+
+        # Batch Normalization for Embedding Differences
+        self.bn_rc = nn.BatchNorm1d(n_rc_emb, affine=False)
+        self.bn_ln = nn.BatchNorm1d(n_ln_emb, affine=False)
+
+        # Linear Layers for Embedding Differences to Accuracy Mean and Variance
+        # The positive linear layer ensures the estimated function is
+        # monotonic increasing
         self.delta_rc_to_acc = nn.Sequential(
-            PositiveLinear(rc_emb, 4), nn.Tanh(), PositiveLinear(4, 2)
+            PositiveLinear(n_rc_emb, n_delta_neurons),
+            nn.Tanh(),
+            PositiveLinear(n_delta_neurons, 2),
         )
         self.delta_ln_to_acc = nn.Sequential(
-            PositiveLinear(ln_emb, 4), nn.Tanh(), PositiveLinear(4, 2)
+            PositiveLinear(n_ln_emb, n_delta_neurons),
+            nn.Tanh(),
+            PositiveLinear(n_delta_neurons, 2),
         )
+
         self.save_hyperparameters()
 
-    def forward(self, uid, mid):
-        w_ln_ratio = self.ln_ratio_weights[mid]
+    def forward(self, x_uid, x_mid):
+        # Get the LN Ratio for the current beatmap
+        w_ln_ratio = self.w_ln_ratio[x_mid]
 
-        uid_rc_emb = self.uid_rc_emb(uid)
-        mid_rc_emb = self.mid_rc_emb(mid)
-        uid_ln_emb = self.uid_ln_emb(uid)
-        mid_ln_emb = self.mid_ln_emb(mid)
+        # Convert the One-Hot Encoded User ID and Beatmap ID to Embeddings
+        x_uid_rc = self.emb_uid_rc(x_uid)
+        x_mid_rc = self.emb_mid_rc(x_mid)
+        x_uid_ln = self.emb_uid_ln(x_uid)
+        x_mid_ln = self.emb_mid_ln(x_mid)
 
-        rc_emb_delta = uid_rc_emb - mid_rc_emb
-        ln_emb_delta = uid_ln_emb - mid_ln_emb
+        # Calculate the difference between the User and Beatmap ID Embeddings
+        x_rc_delta = x_uid_rc - x_mid_rc
+        x_ln_delta = x_uid_ln - x_mid_ln
 
-        rc_emb = self.rc_emb_bn(rc_emb_delta)
-        ln_emb = self.ln_emb_bn(ln_emb_delta)
+        # Normalize the Embedding Differences
+        x_rc_delta = self.bn_rc(x_rc_delta)
+        x_ln_delta = self.bn_ln(x_ln_delta)
 
-        rc_acc = self.delta_rc_to_acc(rc_emb)
-        ln_acc = self.delta_ln_to_acc(ln_emb)
+        # Convert the Embedding Differences to Accuracy Mean and Variance
+        y_rc = self.delta_rc_to_acc(x_rc_delta)
+        y_ln = self.delta_ln_to_acc(x_ln_delta)
 
-        rc_acc_mean = hardsigmoid(rc_acc[:, 0])
-        rc_acc_var = softplus(rc_acc[:, 1])
-        ln_acc_mean = hardsigmoid(ln_acc[:, 0])
-        ln_acc_var = softplus(ln_acc[:, 1])
+        # Squash the predictions to the range [0, 1]
+        y_rc_mean = hardsigmoid(y_rc[:, 0])
+        y_ln_mean = hardsigmoid(y_ln[:, 0])
 
-        y_mean = rc_acc_mean * (1 - w_ln_ratio) + ln_acc_mean * w_ln_ratio
-        y_var = rc_acc_var * (1 - w_ln_ratio) + ln_acc_var * w_ln_ratio
+        # Make sure the variance is positive
+        y_rc_var = softplus(y_rc[:, 1])
+        y_ln_var = softplus(y_ln[:, 1])
+
+        # Combine the predictions using the LN Ratio
+        y_mean = y_rc_mean * (1 - w_ln_ratio) + y_ln_mean * w_ln_ratio
+        y_var = y_rc_var * (1 - w_ln_ratio) + y_ln_var * w_ln_ratio
+
         return y_mean, y_var
 
     @staticmethod
-    def loss_nll(y_pred_mean, y_pred_var, y, eps=1e-10):
-        return (torch.log(y_pred_var + eps) / 2) + (
-            (y - y_pred_mean) ** 2 / (2 * y_pred_var)
+    def loss_nll(
+        mean: torch.Tensor,
+        var: torch.Tensor,
+        target: torch.Tensor,
+        eps: float = 1e-10,
+    ):
+        """Negative Log Likelihood Loss for Gaussian Distribution
+
+        Args:
+            mean: Mean of the Gaussian Distribution
+            var: Variance of the Gaussian Distribution
+            target: Target Value
+            eps: Epsilon to prevent NaNs. Defaults to 1e-10.
+        """
+        return (
+            (torch.log(var + eps) / 2)
+            + ((target - mean) ** 2 / (2 * var + eps))
+        ).mean()
+
+    def decoded_rmse(
+        self,
+        mean: torch.Tensor,
+        var: torch.Tensor,  # noqa
+        target: torch.Tensor,
+    ):
+        return (
+            nn.MSELoss()(
+                torch.tensor(self.decode_acc(mean)),
+                torch.tensor(self.decode_acc(target)),
+            )
+            ** 0.5
         )
 
-    def training_step(self, batch: Any, batch_idx: int) -> STEP_OUTPUT:
+    def step(
+        self,
+        batch: tuple[torch.Tensor, torch.Tensor, torch.Tensor],
+        loss_fn: Any,
+    ):
         x_uid, x_mid, y = batch
         y_pred_mean, y_pred_var = self(x_uid, x_mid)
-        loss = self.loss_nll(y_pred_mean, y_pred_var, y).mean()
-        self.log("train/nll_loss", loss, prog_bar=True)
+        return loss_fn(y_pred_mean, y_pred_var, y)
+
+    def training_step(
+        self,
+        batch: tuple[torch.Tensor, torch.Tensor, torch.Tensor],
+        batch_idx: int,
+    ):
+        self.log(
+            "train/nll_loss",
+            (loss := self.step(batch, self.loss_nll)),
+            prog_bar=True,
+        )
         return loss
 
-    def validation_step(self, batch: Any, batch_idx: int) -> STEP_OUTPUT:
-        x_uid, x_mid, y = batch
-        y_pred, _ = self(x_uid, x_mid)
-        loss = nn.MSELoss()(
-            torch.tensor(self.decode_acc(y_pred)),
-            torch.tensor(self.decode_acc(y)),
+    def validation_step(
+        self,
+        batch: tuple[torch.Tensor, torch.Tensor, torch.Tensor],
+        batch_idx: int,
+    ):
+        self.log(
+            "val/rmse_loss",
+            (loss := self.step(batch, self.decoded_rmse)),
+            prog_bar=True,
         )
-        self.log("val/rmse_loss", loss**0.5, prog_bar=True)
         return loss
 
-    def test_step(self, batch: Any, batch_idx: int) -> STEP_OUTPUT:
-        x_uid, x_mid, y = batch
-        y_pred, _ = self(x_uid, x_mid)
-        loss = nn.MSELoss()(
-            torch.tensor(self.decode_acc(y_pred)),
-            torch.tensor(self.decode_acc(y)),
+    def test_step(
+        self,
+        batch: tuple[torch.Tensor, torch.Tensor, torch.Tensor],
+        batch_idx: int,
+    ):
+        self.log(
+            "test/rmse_loss",
+            (loss := self.step(batch, self.decoded_rmse)),
+            prog_bar=True,
         )
-        self.log("test/rmse_loss", loss**0.5, prog_bar=True)
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(
-            self.parameters(), lr=0.003  # , weight_decay=1e-5
-        )
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
         scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
             optimizer,
             patience=2,
@@ -122,46 +201,54 @@ class DeltaModel(pl.LightningModule):
         }
 
     def decode_acc(self, x):
-        return self.acc_qt.inverse_transform(x.cpu().reshape(-1, 1))
+        return self.qt_acc.inverse_transform(x.cpu().reshape(-1, 1))
 
     def encode_acc(self, x):
-        return self.acc_qt.transform(x.cpu().reshape(-1, 1)).squeeze()
+        return self.qt_acc.transform(x.cpu().reshape(-1, 1)).squeeze()
 
     def decode_uid(self, x):
-        return self.uid_le.inverse_transform(x)
+        return self.le_uid.inverse_transform(x)
 
     def decode_mid(self, x):
-        return self.mid_le.inverse_transform(x)
+        return self.le_mid.inverse_transform(x)
 
     def encode_uid(self, x):
-        return self.uid_le.transform(x)
+        return self.le_uid.transform(x)
 
     def encode_mid(self, x):
-        return self.mid_le.transform(x)
+        return self.le_mid.transform(x)
 
     @property
     def uid_classes(self) -> np.ndarray:
-        return np.array(self.uid_le.classes_)
+        return np.array(self.le_uid.classes_)
 
     @property
     def mid_classes(self) -> np.ndarray:
-        return np.array(self.mid_le.classes_)
+        return np.array(self.le_mid.classes_)
 
     def predict_all(self) -> tuple[pd.DataFrame, pd.DataFrame]:
+        """Predicts the accuracy for all user-beatmap pairs
+
+        Returns:
+            DataFrames of the mean and variance of the accuracy respectively.
+        """
         n_uid = len(self.uid_classes)
         n_mid = len(self.mid_classes)
-        a = torch.cartesian_prod(torch.arange(n_uid), torch.arange(n_mid))
-        uids = a[:, 0]
-        mids = a[:, 1]
-        accs = self.acc_qt.inverse_transform(
-            self(uids, mids)[0].detach().numpy().reshape(-1, 1)
+
+        # Construct the Cartesian Product of User and Beatmap IDs
+        cart = torch.cartesian_prod(torch.arange(n_uid), torch.arange(n_mid))
+        x_uid = cart[:, 0]
+        x_mid = cart[:, 1]
+
+        y_mean = self.qt_acc.inverse_transform(
+            self(x_uid, x_mid)[0].detach().numpy().reshape(-1, 1)
         ).reshape(n_uid, n_mid)
-        accs_var = self.acc_qt.inverse_transform(
-            self(uids, mids)[1].detach().numpy().reshape(-1, 1)
+        y_var = self.qt_acc.inverse_transform(
+            self(x_uid, x_mid)[1].detach().numpy().reshape(-1, 1)
         ).reshape(n_uid, n_mid)
 
         return pd.DataFrame(
-            accs, columns=self.mid_classes, index=self.uid_classes
+            y_mean, columns=self.mid_classes, index=self.uid_classes
         ), pd.DataFrame(
-            accs_var, columns=self.mid_classes, index=self.uid_classes
+            y_var, columns=self.mid_classes, index=self.uid_classes
         )
