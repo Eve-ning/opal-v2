bfdecaedbe0032419c7ad3fd4a4d0f0acde599bd
Evening <dev_evening@hotmail.com>
2024-03-12 13:07:55 +0800

    Add prog bars and sample creation fn

--- opal/create_dataset.py
+++ opal/create_dataset.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 from opal.utils import db_conn
 
@@ -117,29 +118,73 @@ def write_dataset(df, conn):
 
 def create_dataset():
     conn = db_conn()
+    t = tqdm(total=5)
+
+    t.set_description("Reading maps")
     df_maps = read_maps(conn)
+    t.update(1)
+
+    t.set_description("Reading plays")
     df_plays = read_plays(conn)
+    t.update(1)
+
+    t.set_description("Reading users")
     df_users = read_players(conn)
-    df = merge(df_maps, df_plays, df_users)
-    df = (
-        df.merge(
-            df["mid"].value_counts(normalize=True).rename("p_mid"),
-            left_on="mid",
-            right_index=True,
-        )
-        .merge(
-            df["uid"].value_counts(normalize=True).rename("p_uid"),
-            left_on="uid",
-            right_index=True,
-        )
-        .assign(
-            prob=lambda x: x["p_mid"] * x["p_uid"],
-            rand=lambda x: np.random.uniform(0, 1, len(x)),
-        )
+    t.update(1)
+
+    t.set_description("Merging")
+    df = merge(df_maps, df_plays, df_users).assign(
+        rand=lambda x: np.random.uniform(0, 1, len(x)),
     )
+    t.update(1)
+
+    t.set_description("Writing dataset")
     write_dataset(df, conn)
+    t.update(1)
+
+    t.close()
     conn.dispose()
 
 
+def create_sampled_datasets():
+    """Create sampled datasets for testing purposes"""
+
+    # Read from `osu_dataset` and use the `rand` column to sample:
+    # - 1% of the dataset as `osu_dataset_sample_1`
+    # - 10% of the dataset as `osu_dataset_sample_10`
+
+    conn = db_conn()
+    t = tqdm(total=3)
+
+    t.set_description("Reading dataset")
+    df = pd.read_sql(
+        r"SELECT * FROM osu.osu_dataset",
+        conn,
+    )
+    t.update(1)
+
+    t.set_description("Creating samples")
+    df_1 = df.sample(frac=0.01, random_state=42)
+    df_10 = df.sample(frac=0.10, random_state=42)
+    t.update(1)
+
+    t.set_description("Writing samples")
+    df_1.to_sql(
+        "osu_dataset_sample_1",
+        conn,
+        schema="osu",
+        if_exists="replace",
+    )
+    df_10.to_sql(
+        "osu_dataset_sample_10",
+        conn,
+        schema="osu",
+        if_exists="replace",
+    )
+    t.update(1)
+    t.close()
+
+
 if __name__ == "__main__":
-    create_dataset()
+    # create_dataset()
+    create_sampled_datasets()
